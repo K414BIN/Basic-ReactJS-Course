f71b1f1d3043b901286df09310fe386da3efc00b
k414bin <k414bin@yandex.ru>
2021-08-25 23:11:34 +0300

    Under construction this homework

--- src/App.js
+++ src/App.js
@@ -1,86 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import MessageList from './MessageList';
-import MessageInput from './MessageInput';
-import  ChatList from './ChatList';
-import {makeStyles} from '@material-ui/core/styles';
-import "./App.css";
-
-const BotName = "ROBOT";
-const me="Alex";
-
-const useStyles = makeStyles((theme) =>({
-  appWrapper: {
-    height: "100vh",
-    width: "100vw",
-    display: "flex",
-    justifyContent: "center",
-    alignItems:"center"
-  },
-  componentWrapper : {
-    width: "600px",
-    height: "800px",
-    border: "1px solid black",
-    display: "flex",
-    flexDirection: "column"
-  },
-   button : {
-      margin:"0px 10px"
-    },
-    input :{
-      margin:"0px 10px",
-      width: "70%",
-  }
-}));
-
-
-
-function App() {
-  const classes = useStyles();
-  const [inputMessage, setInputMessage] = useState('');
-  const [messagesArray, setMessagesArray] = useState([]);
-  const [chatArray, setChatArray] = useState([]);
-
-  const onSendMessage = () => {
-    const trimmedMessageText = inputMessage.trim();
-
-    if (trimmedMessageText !== '') {
-      setMessagesArray(prev => [...prev,
-      {
-        trimmedMessageText,
-        author: me,
-        time: new Date().toLocaleTimeString(),
-      },
-      ]);
-      setInputMessage('');
-
-      setTimeout(() => {
-        setMessagesArray(prev => [...prev,
-          {
-            trimmedMessageText :  'Сообщение отправлено!',
-            author: BotName,
-            time: new Date().toLocaleTimeString(),
-          },
-        ]);
-      }, 1500);
-      }
-  };
-  useEffect(() => {
-    document.getElementsByClassName("messageList")[0].scrollTop = 9999;
-  });
-  useEffect(() => {
-    if (messagesArray.length > 0) {
-      setTimeout(() => {
-           }, 1500);
-    };
-  }, [setMessagesArray, messagesArray]);
-
-  return( <div className={classes.appWrapper}>
-          <ChatList chatArray={ chatArray } />
-                 <div className={classes.componentWrapper}>
-                   <MessageList messagesArray={messagesArray}  />
-                   <MessageInput inputMessage={inputMessage} setInputMessage={setInputMessage} onSendMessage={onSendMessage} />
-                </div >
-           </div >
-    );
-}
-export default App;
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Chat from './Chat';
+const App = () =>{
+    return (
+    <Router>
+        <Switch>
+                <Route  path="chat"></Route>
+                <Route  path="/"></Route>
+        </Switch>        
+        </Ruter>
+                );
+};
