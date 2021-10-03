5ec57af12a7910d24841897b3c629c2e15c6aabf
k414bin <k414bin@yandex.ru>
2021-10-03 17:03:46 +0300

    Final version

--- src/App.js
+++ src/App.js
@@ -1,33 +1,72 @@
-import React, {useState, useCallback, useEffect, useRef} from "react";
-import './App.css';
-import InputComponent from './InputComponent';
-import MessageList from "./MessageList";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CustomRoute from "./util/CustomRoute";
+import Chat from "./Chat";
+import Cats from "./Cats";
+import Home from "./Home";
+import Signup from "./Auth/Signup";
+import Login from "./Auth/Login";
+import { makeStyles } from "@material-ui/core/styles";
+import firebase from "firebase/compat";
+import { useAuthState } from "react-firebase-hooks/auth";
+import {useContext} from "react";
+import {MyDataContext} from "./index";
+import Profile from "./Profile";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  mainWrapper: {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+  },
+}));
+const firebaseConfig = {
+  apiKey: "AIzaSyAeI-rWuYNYAmru-Q5rDwYnqur5hptH_UE",
+  authDomain: "gb-react-project-e5529.firebaseapp.com",
+  databaseURL: "https://gb-react-project-e5529-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "gb-react-project-e5529",
+  storageBucket: "gb-react-project-e5529.appspot.com",
+  messagingSenderId: "486781428280",
+  appId: "1:486781428280:web:50feee19727b6844ce7ccd"
+};
 
-    const [messageArray, setMessageArray] = useState([]);
-    const onSendMessage = (messageText) =>{
-        setMessageArray((prev) => [...prev,[messageText]
-        /* не заработало!
-            {
-            messageText,
-            author: 'me'
-            }
-            */
-        ]);
-     };
-    useEffect(() =>{
-        if ( messageArray.length > 0 && messageArray.author !=="ROBOT" ) {
-                setTimeout(() => {
-                    let messageText = "Hello, human!";
-                    console.log(messageText)}  , 1000);
-            }}, [messageArray]);
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.database();
 
-    return (
-        <div className="mainWrapper">
-            <MessageList messagesArray={messageArray}/>
-            <InputComponent onSendMessage={onSendMessage}/>
+const App = () => {
+  const classes = useStyles();
+  const appVer = useContext(MyDataContext);
+
+  return (
+      <Router>
+        <div className={classes.mainWrapper}>
+
+          <Switch>
+            <CustomRoute secured path="/chat/:id">
+              <Chat />
+            </CustomRoute>
+
+            <CustomRoute secured withAppBar={true} path="/profile">
+              <Profile />
+            </CustomRoute>
+
+            <Route path="/cats" secured withAppBar={true}>
+              <Cats />
+            </Route>
+
+            <Route path="/login">
+              <Login />
+            </Route>
+
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <CustomRoute secured withAppBar={true}  path="/" >
+              <Home />
+            </CustomRoute>
+          </Switch>
         </div>
-    );}
+      </Router>
+  );
+};
 
 export default App;
